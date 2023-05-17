f3f29c3836acd5ede80927358bab44902140950c
kavish shah <kavishshah016@gmail.com>
2023-05-18 00:17:45 +0530

    Updates

---
+++ precache-manifest.92a83258fa27896ce50f2228e622a730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d26b7a642afd3f21d4",
+    "url": "/to_do_list/static/css/main.945e4103.chunk.css"
+  },
+  {
+    "revision": "56d26b7a642afd3f21d4",
+    "url": "/to_do_list/static/js/main.56d26b7a.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/to_do_list/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "2f5580ece8b36ea31250",
+    "url": "/to_do_list/static/js/2.2f5580ec.chunk.js"
+  },
+  {
+    "revision": "72919e2bc2ef87dc3ada",
+    "url": "/to_do_list/static/js/runtime~main.72919e2b.js"
+  },
+  {
+    "revision": "f604e2ac95bce283d3566cec79d5a661",
+    "url": "/to_do_list/index.html"
+  }
+];
